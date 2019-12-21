ad7a36a0a11feb7a7f1a9339c15351011936b84e
thandi1908 <56547759+thandi1908@users.noreply.github.com>
2019-12-21 21:55:45 +0000

    Add files via upload
    Rule number two: set separation updated and now working! (boid.py)

--- Window.py
+++ Window.py
@@ -1,23 +1,24 @@
 import sys
 import pygame
-from boid import spawn
-from boid import rule_one
-from boid import rule_two
+from boids.boid import spawn
+from boids.boid import rule_one
+from boids.boid import rule_two
+from boids.boid import Boid
 
 from boids.boid import rule_three
-
-from TwoVector import TwoVector as Vec
+from boids.boid import Boid
+from boids.TwoVector import TwoVector as Vec
 import time
 
 
 def run_game():
     # Initialise game and create screen object
-    screen_height = 600
-    screen_width = 600
+    screen_height = 700
+    screen_width = 800
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Boids")
-    screen.fill((144, 153, 206))
+    screen.fill((0, 0, 0))
     boid_list = spawn(screen)
 
     while True:
@@ -26,21 +27,20 @@ def run_game():
         for obj in boid_list:
             obj.blitme()
 
-
-
-        #rule_one(boid_list)
+        rule_one(boid_list)
         rule_two(boid_list)
-        #rule_three(boid_list)
+        rule_three(boid_list)
 
         for i in boid_list:
+            i.wraparound(screen_width,screen_height)
             i.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        time.sleep(0.01)
+        #time.sleep(0.01)
         pygame.display.flip()
-        screen.fill((144, 153, 206))
+        screen.fill((0, 0, 0))
 
 
 run_game()
--- boid.py
+++ boid.py
@@ -7,8 +7,8 @@ from random import randrange
 class Boid(object):
 
     def __init__(self, screen):
-        self.position = Vec(randrange(500), randrange(500))
-        self.velocity = Vec(randrange(-1, 1, 1), randrange(-1, 1, 1))
+        self.position = Vec(randrange(800), randrange(700))
+        self.velocity = Vec(randrange(-2, 2, 1), randrange(-2, 2, 1))
         self.image = pygame.image.load("icons8-triangle-96.png")
         self.screen = screen
         self.rect = self.image.get_rect()
@@ -21,11 +21,23 @@ class Boid(object):
         self.position.x += self.velocity.x
         self.position.y += self.velocity.y
 
+    # Method from processing.org/flocking
+    def wraparound(self, width, height):
+        r = 2
+        if self.position.x < -r:
+            self.position.x = width + r
+        if self.position.y < -r:
+            self.position.y = height + r
+        if self.position.x > width + r:
+            self.position.x = -r
+        if self.position.y > height + r:
+            self.position.y = -r
+
 
 def spawn(screen):
     boid_list = []
     # creating 10 boids
-    for i in range(10):
+    for i in range(100):
         boid = Boid(screen)
         boid_list.append(boid)
     return boid_list
@@ -42,11 +54,13 @@ def rule_one(boid_list):
                 delta_x += boid_list[j].position.x / (list_len - 1)
                 delta_y += boid_list[j].position.y / (list_len - 1)
 
-        new_x = (boid_list[i].position.x - delta_x) / 1000
-        new_y = (boid_list[i].position.y - delta_y) / 1000
-        new_pos.append(Vec(boid_list[i].velocity.x - new_x, boid_list[i].velocity.y - new_y))
+        new_x = (boid_list[i].position.x - delta_x) / 300
+        new_y = (boid_list[i].position.y - delta_y) / 300
+        new_pos.append(Vec(new_x, new_y))
+
     for i in range(len(new_pos)):
-        boid_list[i].velocity = new_pos[i]
+        boid_list[i].velocity.x -= new_pos[i].x
+        boid_list[i].velocity.y -= new_pos[i].y
 
 
 def rule_two(boid_lis):
@@ -58,29 +72,35 @@ def rule_two(boid_lis):
             if i != j:
                 diff_pos = boid_lis[i].position.subtract(boid_lis[j].position)
                 mod = diff_pos.dot_product(diff_pos)
-                if mod**0.5 < 1000:
-                    zero.subtract((boid_lis[i].position.subtract(boid_lis[j].position)))
+                if mod < 1000:
+                    diff = boid_lis[i].position.subtract(boid_lis[j].position)
+                    zero = zero.subtract(diff)
+                    zero = zero.scale(10)
 
         c_list.append(zero)
-    for i in range(ran):
-        boid_lis[i].velocity = boid_lis[i].velocity.subtract(c_list[i])
 
+
+    for i in range(ran):
+        boid_lis[i].velocity.x -= c_list[i].x
+        boid_lis[i].velocity.y -= c_list[i].y
 
 
 def rule_three(boid_list):
     list_len = len(boid_list)
+    norm_len = list_len - 1
     new_vel = []
     for i in range(list_len):
         delta_x = 0
         delta_y = 0
         for j in range(list_len):
             if i != j:
-                delta_x += boid_list[j].velocity.x / (list_len - 1)
-                delta_y += boid_list[j].velocity.y / (list_len - 1)
+                delta_x += boid_list[j].velocity.x / norm_len
+                delta_y += boid_list[j].velocity.y / norm_len
 
-        new_x = (boid_list[i].velocity.x - delta_x) / 100
-        new_y = (boid_list[i].velocity.y - delta_y) / 100
-        new_vel.append(Vec(boid_list[i].velocity.x + new_x, boid_list[i].velocity.y + new_y))
+        new_x = (boid_list[i].velocity.x - delta_x)/8
+        new_y = (boid_list[i].velocity.y - delta_y)/8
+        new_vel.append(Vec(new_x, new_y))
+
     for i in range(len(new_vel)):
-        boid_list[i].velocity = new_vel[i]
-
+        boid_list[i].velocity.x -= new_vel[i].x
+        boid_list[i].velocity.y -= new_vel[i].y
